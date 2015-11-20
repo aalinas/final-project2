636ddfdc6c856a31a45d8e05a2aa2c0cf0ddb494
Graphic <graphic@SOD-SJH-516-21M.local>
2015-11-20 14:53:51 -0500

    in class

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 
-$("#hide").click(function(){
-        $("p").hide();
-    });
 
-$("#show").click(function(){
-        $("p").show();
-    });
+ $(".instructions-button").click(function() { // click button to show instructions // 
+ 	$(this).getElementById('.instructions p')
+ });
+
+$(".about-button").click(function() { // click button to show about the game // 
+ 	 $(this).getElementById('.about p')
+ });
+
+$("p").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+
+$(".new-game-button").click(function() { // click button to start a new game // 
+
+ });
+
+
+
+$(".score-button").click(function() { // click button to show score // 
+
+ });
