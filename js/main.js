41018bca0940e26816b3eb2c19b833d2a8479c93
Angela Alinas <angelaalinas@gmail.com>
2015-12-09 18:04:04 -0500

    commit-2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,105 @@
+    
+$('button.start').on("click", function() {
+    bg.load();
+    bg.play();  
+
+    function animateNotes(item$) { // animates notes
+    var w = Math.min($(window).width(), $("#gamescreen").width());
+    var r = Math.floor(Math.random()*2000) + 2000;
+    item$.animate({top: (w + item$.width()) + "px"}, r, 'linear', function() {
+        item$.css("top", -item$.width());    
+        animateNotes(item$);  
+        drumaudio1.load();
+        drumaudio1.play();      
+    });
+    } 
+    $('.box').each(function() {
+    animateNotes($(this));
+    });
 
 
- $(".instructions-button").click(function() { // click button to show instructions // 
- 	$(this).getElementById('.instructions p')
- });
-
-$(".about-button").click(function() { // click button to show about the game // 
- 	 $(this).getElementById('.about p')
- });
-
-$("p").click(function(){
-    alert("The paragraph was clicked.");
 });
 
 
-$(".new-game-button").click(function() { // click button to start a new game // 
+$('button.restart').on("click", function() {
+    location.reload(true);  //reload page 
+});
 
- });
+
+// var drum1 = document.getElementById('drum1');
+
+$('body').on('keydown', function (e) {
+
+    if (e.keyCode == 65) {  // a   
+         $('#drum1').css('background-image',"url(img/drum1hit.png)"); 
+         drumaudio1.load();
+         drumaudio1.play();
+         
+    }
+
+    if (e.keyCode  == 83) {  // s
+         $("#drum2").css('background-image',"url(img/drum2hit.png)"); 
+         drumaudio2.load();
+         drumaudio2.play();
+     }
+
+     if (e.keyCode  == 75) {  // k
+         $("#drum3").css('background-image',"url(img/drum2hit.png)"); 
+         drumaudio3.load();
+         drumaudio3.play();
+     }
+
+     if (e.keyCode  == 76) {  // l
+         $("#drum4").css('background-image',"url(img/drum1hit.png)"); 
+        drumaudio4.load();
+         drumaudio4.play();
+     }
 
 
 
-$(".score-button").click(function() { // click button to show score // 
+});
 
- });
+$('body').on('keyup', function (e) {
+
+    if (e.keyCode == 65) {  // a
+         $('#drum1').css('background-image',"url(img/drum1.png)"); 
+    }
+
+    if (e.keyCode  == 83) {  // s
+         $("#drum2").css('background-image',"url(img/drum2.png)"); 
+     }
+
+     if (e.keyCode  == 75) {  // k
+         $("#drum3").css('background-image',"url(img/drum2.png)"); 
+     }
+
+     if (e.keyCode  == 76) {  // l
+         $("#drum4").css('background-image',"url(img/drum1.png)"); 
+     }
+
+
+});
+
+//drum audio
+var drum1 = document.getElementById('drumaudio1');
+var drum2 = document.getElementById('drumaudio2');
+var drum3 = document.getElementById('drumaudio3');
+var drum4 = document.getElementById('drumaudio4');
+
+//bg audio
+var bgaudio = document.getElementById('bg');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
